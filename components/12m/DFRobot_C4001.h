248b57db938dfc4d28bd16c1e55b0ecdab411afc
specanything <47363825+specanything@users.noreply.github.com>
2024-07-19 17:14:49 -0400

    Update DFRobot_C4001.h

--- components/12m/DFRobot_C4001.h
+++ components/12m/DFRobot_C4001.h
@@ -8,6 +8,20 @@
  * @date 2023-02-02
  * @url https://github.com/DFRobot/DFRobot_C4001
  */
+
+#include "esphome/components/uart/uart.h"
+#include "esphome/core/automation.h"
+#include "esphome/core/component.h"
+
+#ifdef USE_BINARY_SENSOR
+#include "esphome/components/binary_sensor/binary_sensor.h"
+#endif
+#ifdef USE_SWITCH
+#include "esphome/components/switch/switch.h"
+#endif
+
+#include "commands.h"
+
 #ifndef __DFROBOT_C4001_H__
 #define __DFROBOT_C4001_H__
 
